cd62477db65e0f35a8aa712a0fe89547198b60ce
abdullahsal702 <88620119+abdullahsal702@users.noreply.github.com>
2024-08-05 03:51:33 -0400

    first commit

---
+++ app/page.js
@@ -0,0 +1,198 @@
+"use client";
+import { useEffect, useState, useRef } from "react";
+
+import {
+  Typography,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Box,
+  Button,
+  Modal,
+  TextField,
+} from "@mui/material";
+import RemoveCircleIcon from "@mui/icons-material/RemoveCircle";
+import {
+  collection,
+  doc,
+  getDocs,
+  setDoc,
+  getDoc,
+  deleteDoc,
+} from "firebase/firestore";
+import { db } from "../firebase";
+
+export default function Home() {
+  const [inventory, setInventory] = useState([]);
+  const [itemName, setItemName] = useState("");
+  const [open, setOpen] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const itemRefs = useRef([]);
+
+  const fetchData = async () => {
+    const querySnapshot = await getDocs(collection(db, "inventory"));
+    const inventoryList = [];
+    querySnapshot.forEach((doc) => {
+      inventoryList.push({ name: doc.id, ...doc.data() });
+    });
+    setInventory(inventoryList);
+  };
+
+  const addItem = async (item) => {
+    const docRef = doc(db, "inventory", item);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      await setDoc(docRef, { quantity: docSnap.data().quantity + 1 });
+    } else {
+      await setDoc(docRef, { quantity: 1 });
+    }
+
+    fetchData();
+  };
+
+  const removeItem = async (item) => {
+    const docRef = doc(db, "inventory", item);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      const { quantity } = docSnap.data();
+      if (quantity == 1) {
+        await deleteDoc(docRef);
+      } else {
+        await setDoc(docRef, { quantity: docSnap.data().quantity - 1 });
+      }
+    }
+
+    fetchData();
+  };
+
+  const handleSearch = () => {
+    const itemIndex = inventory.findIndex((item) =>
+      item.name.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    if (itemIndex !== -1 && itemRefs.current[itemIndex]) {
+      itemRefs.current[itemIndex].scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+      });
+    }
+  };
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <Box
+      width="100vw"
+      height="100vh"
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      gap={2}
+    >
+      {/*  */}
+      <Modal open={open} onClose={handleClose}>
+        <Box
+          position="absolute"
+          top="50%"
+          left="50%"
+          transform="translate(50%,50%)"
+          width={400}
+          border="2px solid #000"
+          boxShadow={24}
+          bgcolor={"white"}
+          p={4}
+          display="flex"
+          flexDirection="column"
+          gap={3}
+          sx={{ transform: "translate(-50%,-50%)" }}
+        >
+          <Typography>Add Item</Typography>
+          <TextField
+            label="Item Name"
+            variant="outlined"
+            value={itemName}
+            onChange={(e) => {
+              setItemName(e.target.value);
+            }}
+          />
+          <Button
+            onClick={() => {
+              addItem(itemName);
+              setItemName("");
+              handleClose();
+            }}
+          >
+            Add
+          </Button>
+        </Box>
+      </Modal>
+      {/*  */}
+      <Typography variant="h2">Pantry Tracker</Typography>
+      <Box width="20%" display="flex" flexDirection="column" gap={2}>
+        <TextField
+          label="Search Item"
+          variant="outlined"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <Button variant="contained" onClick={handleSearch}>
+          Search
+        </Button>
+      </Box>
+      <Box width="50%">
+        <TableContainer component={Paper} elevation={2} sx={{ maxHeight: 300 }}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Item</TableCell>
+                <TableCell>Quantity</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody sx={{ maxHeight: 200, overflow: "auto" }}>
+              {inventory.map((item, index) => (
+                <TableRow
+                  key={index}
+                  ref={(el) => (itemRefs.current[index] = el)}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {item.name}
+                  </TableCell>
+                  <TableCell
+                    sx={{
+                      display: "flex",
+                      flexDirection: "row",
+                      alignItems: "center",
+                    }}
+                  >
+                    <Box>{item.quantity}</Box>
+                    <RemoveCircleIcon
+                      sx={{ color: "#ff0000" }}
+                      onClick={() => {
+                        removeItem(item.name);
+                      }}
+                    />
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <Button variant="contained" onClick={handleOpen} sx={{ marginTop: 2 }}>
+          Add Item
+        </Button>
+      </Box>
+    </Box>
+  );
+}
---
+++ firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBUn7sUrX6E03R0xQXiyY88hqNLac8STgw",
+  authDomain: "pantry-f8287.firebaseapp.com",
+  projectId: "pantry-f8287",
+  storageBucket: "pantry-f8287.appspot.com",
+  messagingSenderId: "340429232223",
+  appId: "1:340429232223:web:5a9ef5d216a7416f5723ea",
+  measurementId: "G-R0DYLWHJK6",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const db = getFirestore(app);
